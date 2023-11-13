dbb4aa984d43a7932b5732d9f3357b3089b8221f
nicolas <n.tiesenhausen@gmail.com>
2023-11-13 12:52:51 +0100

    Feat - Add - Color pallet and device info

---
+++ utils/colors.js
@@ -0,0 +1,10 @@
+export const colors = {
+  white: '#fff',
+  grey: '#e1e5f2',
+  lightBlue: '#bfdbf7',
+  blue: '#1f7a8c',
+  darkBlue: '#022b3a',
+  black: '#000',
+  red: '#f00',
+  alternativeRed: '#ba181b',
+};
