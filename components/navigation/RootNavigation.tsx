07ebd39201a0113fc6c1a935500de147ceacd212
nicolas <n.tiesenhausen@gmail.com>
2023-11-23 11:45:25 +0100

    Update -  TabBar style

--- components/navigation/RootNavigation.tsx
+++ components/navigation/RootNavigation.tsx
@@ -11,6 +11,8 @@ import MeteoHome from '../../screens/MeteoHome';
 import { FontAwesome } from '@expo/vector-icons';
 import { Ionicons } from '@expo/vector-icons';
 
+import Color from '../../utils/colors';
+
 const RootNavigation = createBottomTabNavigator();
 const TodoStack = createNativeStackNavigator<RootStackParamList>();
 const MeteoStack = createNativeStackNavigator();
@@ -41,12 +43,22 @@ const MeteoNavigator = (): React.JSX.Element => {
 
 const RootNavigator = (): React.JSX.Element => {
   return (
-    <RootNavigation.Navigator screenOptions={{ headerShown: false }}>
+    <RootNavigation.Navigator screenOptions={{ 
+		headerShown: false,  
+		tabBarStyle: { height: 60 },
+		tabBarActiveBackgroundColor: Color.material, 
+		tabBarInactiveBackgroundColor: Color.material,
+		tabBarLabelStyle: {
+			fontSize: 20,
+			paddingBottom: 1
+		},
+		tabBarActiveTintColor: Color.darkBlue,
+ }}>
       <RootNavigation.Screen
         name="Todo"
         component={TodoNavigator}
         options={{
-          tabBarIcon: () => <FontAwesome name="list" size={24} color="black" />,
+          tabBarIcon: () => <FontAwesome name="list" size={24} color={Color.darkBlue} />,
         }}
       />
       <RootNavigation.Screen
