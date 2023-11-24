940a3f5e1629287dacf17e1836b15ca1a6d6d17a
nicolas <n.tiesenhausen@gmail.com>
2023-11-24 13:14:25 +0100

    fix - type error

--- components/TodoForm/DatePicker.tsx
+++ components/TodoForm/DatePicker.tsx
@@ -35,7 +35,7 @@ const DatePicker = ({ updateDate, dateToDisplay }) => {
         <DateTimePicker
           testID="dateTimePicker"
           value={date}
-          mode={Platform.OS === 'ios' ? mode : 'date'}
+          //mode={Platform.OS === 'ios' ? mode : 'date'}
           is24Hour={true}
           onChange={onChange}
         />
